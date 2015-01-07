8d1ce5d275c1cf5556faa51932f905b37345dbcd
jgbarcos <javiergarciabarcos@gmail.com>
2015-01-07 14:56:47 +0100

    Add SameApplicationTests.java for @DirtiesContext problem debugging

---
+++ goldenBrown/src/test/java/urlshortener2014/goldenbrown/SameApplicationTests.java
@@ -0,0 +1,34 @@
+package urlshortener2014.goldenbrown;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import urlshortener2014.goldenbrown.Application;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port=0")
+@DirtiesContext
+public class SameApplicationTests {
+
+	@Value("${local.server.port}")
+	private int port = 0;
+
+	@Test
+	public void testAlwaysOk() throws Exception {
+
+	}
+}
